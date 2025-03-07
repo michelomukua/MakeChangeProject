5b4200e39a957eaa234b922895b28226f8846435
Michel <michelmukua@THOMAS-s-S23-FE.localdomain>
2025-03-07 15:53:05 -0700

    first commit

---
+++ src/MakeChange/CashRegister.java
@@ -0,0 +1,15 @@
+package MakeChange;
+
+import java.util.Scanner;
+
+public class CashRegister {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double amountDue;
+		double amountPaid;
+		double changeDue;
+	}
+
+}
